74f5e7ccd63982be84b890629ee5999e8bb0c1e8
habyy-sans <muhammadhasbiyashshiddiqi@gmail.com>
2024-12-13 11:21:44 +0700

    one

--- caseStudy2Group5.java
+++ caseStudy2Group5.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class caseStudy2Group5 {
 
     static int select;
+    static int index = 0;
     public static void main(String[] args) {
         String [][] costumer = new String[10][4];
         mainMenu(costumer);
@@ -30,6 +31,26 @@ public class caseStudy2Group5 {
     }
 
     static void addOrder(String[][]costumer){
+        Scanner sc = new Scanner(System.in);
+        if (index < costumer.length){
+            System.out.print("enter costumer name: ");
+            costumer[index][0] = sc.nextLine();
+            System.out.print("enter number table: ");
+            costumer[index][1] = sc.nextLine();
+            System.out.println();
+        }
+        String menuName;
+        menuName = sc.nextLine();
+        System.out.println("==== MENU KAFE ====");
+        System.out.println("1. kopi hitam - Rp 15.000");
+        System.out.println("2. latte - Rp 22.000");
+        System.out.println("3. teh tarik - Rp 12.000");
+        System.out.println("4. mie goreng - Rp 18.000");
+        costumer[index][2] = menuName;
+    
+        while (true){
+            
+        }
 
     }
 
